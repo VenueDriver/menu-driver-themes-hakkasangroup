43de658a82e81f065e41c2ee5c1b530ce0cd7dfb
Ryan Alyn Porter <rap@endymion.com>
2020-11-06 14:47:45 -0500

    WIP

---
+++ update-all.sh
@@ -0,0 +1,5 @@
+bundle exec exe/menudriver generate sake-no-hana  --theme=sake-no-hana.theme &&
+bundle exec exe/menudriver generate herringbone-0  --theme=herringbone.theme &&
+bundle exec exe/menudriver generate casa-calavera --theme=casa-calavera.theme &&
+bundle exec exe/menudriver generate yauatcha-riyadh --theme=hakkasangroup.theme &&
+bundle exec exe/menudriver generate searsucker-2  --theme=searsucker.theme && tput bel
